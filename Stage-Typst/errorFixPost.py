22600c62e4229cb52c042f973073675ca25397be
Ali Adam Labadi <aliadam.labadi@bootlin.com>
2026-03-16 16:07:26 +0100

    Update - Day 1 - Week 3

--- Stage-Typst/errorFixPost.py
+++ Stage-Typst/errorFixPost.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 import sys
 import re
+import subprocess
 from pathlib import Path
+
 
 def relecture():
     patternImg = re.compile(r'#image\("([^"]+)"')
 
     for ligne in sys.stdin:
+        
 
         match = patternImg.search(ligne)
         if match:
@@ -14,14 +17,20 @@ def relecture():
             filename = Path(path).name
             ligne = ligne.replace(path, filename)
         
-        ligne = ligne.replace("""“‘""", "``` ")
+        ligne = ligne.replace("""“‘""", "```")
         ligne = ligne.replace("\\[fragile\\]", "")
+        if ligne.startswith("== "):
+            ligne = ligne.replace("== ", "= ")
         if ligne.startswith("= "):
-            ligne = ligne.replace("=", """#import "@local/bootlin:0.1.0": *\n#import "@local/bootlin-yocto:0.1.0": *\n#import "@local/bootlin-utils:0.1.0": *\n\n#show: bootlin-theme.with(\n  aspect-ratio: "16-9",\n  config-info(\n  title: [#text(size: 0.9em)[Yocto Project and OpenEmbedded:\\ Recent Changes and Future Directions]],\n  author: [#v(-30pt)#text(size: 0.8em)[Antonin Godard\\ _antonin.godard\\@bootlin.com_]],\n  date: [#v(-30pt)#text(size: 0.8em)[Embedded Recipes 2025]],\n  institution: [Bootlin],\n),\nconfig-common(\n  // Compile with `typst c --input handout=1 ...` to generate the handout.\n  handout: "handout" in sys.inputs and sys.inputs.handout == "1",\n))\n \n#show raw.where(block: false): r => { text(fill: color-link)[#r] } \n =""")
+            ligne = ligne.replace("=", """#import "@local/bootlin:0.1.0": *\n#import "@local/bootlin-yocto:0.1.0": *\n#import "@local/bootlin-utils:0.1.0": *\n\n#show: bootlin-theme.with(\n  aspect-ratio: "16-9",\n\nconfig-common(\n  // Compile with `typst c --input handout=1 ...` to generate the handout.\n  handout: "handout" in sys.inputs and sys.inputs.handout == "1",\n))\n#show raw.where(block: true): set block(fill: luma(240), inset: 1em, radius:0.5em, width:100%)\n#show raw.where(block: false): r => { text(fill: color-link)[#r] } \n\n=""")
+        
         ligne = ligne.replace("\\=\\=\\=", "=== ")
 
+        if "```make" in ligne :
+            ligne = ligne.replace("```make", "``` make")
+        
 
-        if "height" in ligne or "width" in ligne:
+        if "textheight" in ligne or "textwidth" in ligne:
             if "#image" in ligne:
                 nv_ligne = ligne.replace("\\textheight", "0%")
                 nv_ligne = nv_ligne.replace("0.", "")
---
+++ slides/audio-alsa-utils/audio-alsa-utils.typ
@@ -0,0 +1,64 @@
+#import "@local/bootlin:0.1.0": *
+#import "@local/bootlin-yocto:0.1.0": *
+#import "@local/bootlin-utils:0.1.0": *
+
+#show: bootlin-theme.with(
+  aspect-ratio: "16-9",
+
+config-common(
+  // Compile with `typst c --input handout=1 ...` to generate the handout.
+  handout: "handout" in sys.inputs and sys.inputs.handout == "1",
+))
+#show raw.where(block: true): set block(fill: luma(240), inset: 1em, radius:0.5em, width:100%)
+#show raw.where(block: false): r => { text(fill: color-link)[#r] } 
+
+= alsa-utils
+<alsa-utils>
+===  alsa-utils
+
+- ```alsa-utils``` is a repository of tools to interact with ALSA devices
+
+- #link("https://github.com/alsa-project/alsa-utils")
+
+===  Controls
+
+- ```alsamixer``` provides a ```ncurse``` based graphical interface to
+  modify sound cards controls.
+
+- ```amixer``` is a command line tool to set controls.
+
+  - The ```scontrols``` and ```controls``` commands list the available
+    controls.
+
+  - The ```contents``` commands list the available controls and shows
+    their content.
+
+  - The ```cset``` and ```sset``` commands allows modifying the controls.
+
+  - The ```cget``` and ```sget``` commands show the content of a specific
+    control.
+
+- ```alsactl``` is a tool that can save the control values to a file and
+  restore them from a file.
+
+===  Playback and capture
+
+- ```speaker-test``` can generate tones or noises to play on specific
+  channels with a specified rate.
+
+- ```aplay``` plays an audio file. It is able to set many stream
+  parameters.
+
+- ```arecord``` can record an audio stream to a file.
+
+``` Using userspace tools to:
+
+- configure sound card controls
+
+- load and store default values for controls
+
+- play sound
+
+- record
+
+```
